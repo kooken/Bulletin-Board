211fad9ee75cdb04ec723a3ce5f2eed88e29b956
mariasazhina <msazhina23@gmail.com>
2024-08-17 20:22:49 +0300

    change readme and created tests

--- board/tests.py
+++ board/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+from board.models import Ad, Comment
 
-# Create your tests here.
+User = get_user_model()
+
+@pytest.mark.django_db
+class AdTestCase:
+
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(email="user@example.com", password="password")
+        self.client.force_authenticate(user=self.user)
+        self.ad = Ad.objects.create(
+            title="Test Ad",
+            price=100,
+            author=self.user,
+            description="This is a test ad"
+        )
+
+    def test_ad_creation(self):
+        url = reverse("ad-list")
+        data = {
+            "title": "New Ad",
+            "price": 200,
+            "description": "New ad description"
+        }
+        response = self.client.post(url, data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Ad.objects.count() == 2
+        assert Ad.objects.get(title="New Ad").description == "New ad description"
+
+    def test_ad_list(self):
+        url = reverse("ad-list")
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data['results']) == 1
+        assert response.data['results'][0]['title'] == self.ad.title
+
+    def test_ad_update(self):
+        url = reverse("ad-detail", args=[self.ad.pk])
+        data = {
+            "title": "Updated Ad Title"
+        }
+        response = self.client.patch(url, data)
+        assert response.status_code == status.HTTP_200_OK
+        assert Ad.objects.get(pk=self.ad.pk).title == "Updated Ad"
--- config/settings.py
+++ config/settings.py
@@ -191,3 +191,16 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/users/login/'
+
+SWAGGER_SETTINGS = {
+   'SECURITY_DEFINITIONS': {
+      'Basic': {
+            'type': 'basic'
+      },
+      'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+      }
+   }
+}
